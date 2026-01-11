76d3358a45248e74a32d3bb34e76ad66a1e7c09d
WILLIAM JAMES <161729235+williamjames2004@users.noreply.github.com>
2026-01-11 09:18:58 +0530

    Update Admin.js

--- Models/Admin.js
+++ Models/Admin.js
@@ -1,9 +1,32 @@
 const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
-  admin_id: { type: String, unique: true },
-  name: String,
-  password: String
+  admin_id: { 
+    type: String, 
+    unique: true, 
+    required: true 
+  },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  adminType: {
+    type: String,
+    enum: [
+      "President",
+      "VicePresident",
+      "Secretary",
+      "ViceSecretary",
+      "Volunteer",
+      "Professor",
+      "Special"
+    ],
+    required: true
+  }
 });
 
 module.exports = mongoose.model("Admin", adminSchema);
