ff40f580a35af16b0781443a102d6ae6675885ee
WILLIAM JAMES <161729235+williamjames2004@users.noreply.github.com>
2026-02-13 11:56:30 +0530

    Update buzzerScore.js

--- config/buzzerScore.js
+++ config/buzzerScore.js
@@ -1,5 +1,6 @@
 module.exports = {
   C1: 10,   // First correct
   C2: 5,    // Second correct OR first wrong penalty
-  C3: 2     // Second wrong penalty
+  C3: 0     // Second wrong penalty
+
 };
