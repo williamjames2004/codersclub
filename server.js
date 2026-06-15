e5b715212ed064f750265a489a9695599ea065d6
WILLIAM JAMES <161729235+williamjames2004@users.noreply.github.com>
2026-06-15 06:57:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const authRoutes = require("./Routes/authRoutes");
 const adminRoutes = require("./Routes/adminRoutes");
 const quizRoutes = require("./Routes/quizRoutes");
 const buzzerRoutes = require("./Routes/buzzerRoutes");
+const officials = require("./Routes/clubofficials");
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 
 // 👇 expose uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/clubofficials", express.static(path.join(__dirname, "clubofficials")));
 
 /* Connect DB */
 connectDB();
@@ -27,6 +29,7 @@ app.use("/auth", authRoutes);
 app.use("/admin", adminRoutes);
 app.use("/quiz", quizRoutes);
 app.use("/buzzer", buzzerRoutes);
+app.use("/clubofficials", officials);
 
 /* Test Route */
 app.get("/", (req, res) => {
@@ -38,4 +41,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
